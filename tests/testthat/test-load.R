c4303b1a89fcd49338066743fe03f6f71ba5615e
jsta <stachel2@msu.edu>
2019-01-30 21:13:02 -0500

    add failing test, nla 2012 is missing chla_wide

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -13,3 +13,12 @@ test_that("load fails well", {
            stop("Error in data compilation.")),
            "Error in data compilation.")
   })
+
+test_that("data objects are correct dimensions", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  nla <- nla_load(2012)
+  expect_equal("chla_wide" %in% names(nla), TRUE)
+})
