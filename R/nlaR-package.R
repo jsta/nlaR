f4f0c644fcfef95ba3379afbfa163ff2d44f3e8a
jsta <stachel2@msu.edu>
2019-09-11 08:44:28 -0400

    add nlaR alias

--- R/nlaR-package.R
+++ R/nlaR-package.R
@@ -1,6 +1,6 @@
 #' R interface to the National Lakes Assessment
 #' @name nlaR-package
-#' @aliases nla
+#' @aliases nla nlaR
 #' @importFrom utils read.csv unzip
 #' @docType package
 #' @title R interface to the National Lakes Assessment
